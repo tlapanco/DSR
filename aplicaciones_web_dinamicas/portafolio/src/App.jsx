fe02287a25ee44d9eb8435a9d392e078417d5d08
tl <99222342+tlapanco@users.noreply.github.com>
2025-06-16 17:56:07 -0500

    Add: AWD - Portfolio: header section

---
+++ aplicaciones_web_dinamicas/portafolio/src/App.jsx
@@ -0,0 +1,12 @@
+import BarraNavegacion from "./componentes/BarraNavegacion"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+		<BarraNavegacion />
+    </>
+  )
+}
+
+export default App
